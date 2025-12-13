2af731d7d62a211fe5bae1301a47af9f042417e3
Anand Vidvat <anandvidvat@gmail.com>
2025-12-13 01:20:22 -0800

    Add intro_to_toons function explaining TOON advantages
    Introduced a function that explains the benefits of using Token oriented Object Notion (TOON) over JSON for LLMs.

---
+++ tokenization/toons.py
@@ -0,0 +1,10 @@
+
+
+def intro_to_toons():
+  process = """ JSON file format while human readable is not the best format to parse documents and feed it to LLMs, due to poor compression ratios.
+            Hence, an alternative notation, Token oriented Object Notion (TOON) has be introduced which provides better results rather traditional JSON structures.
+            """
+
+  value_add = """ Toons is valuable due to the fact that LLM model usage is depending on $$$ per 1M input tokens or so, 
+                  hence feeding same or more information for lower total input tokens using TOONs is a obvious choice """
+
