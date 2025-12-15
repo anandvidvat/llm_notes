e92c015b13c13fd5dc21f1bddfbff59025f47034
Anand Vidvat <anandvidvat@gmail.com>
2025-12-14 22:45:25 -0800

    Update toons.py

--- tokenization/toons.py
+++ tokenization/toons.py
@@ -1,8 +1,11 @@
 
 
 def intro_to_toons():
-  process = """ JSON file format while human readable is not the best format to parse documents and feed it to LLMs, due to poor compression ratios.
-            Hence, an alternative notation, Token oriented Object Notion (TOON) has be introduced which provides better results rather traditional JSON structures.
+  process = """ JSON file format while human readable since they are verbose, have quotes (",") and special characters which provide visual clarity & human readable formating 
+              but at the expence of more memory to represent same information. JSON is the request and responce format of many RESTful services but is not the best format to parse documents and feed it to LLMs, due to poor compression ratio.
+              Hence, an alternative notation, Token-oriented Object Notion (TOON) has be introduced which provides better results rather traditional JSON structures.
+
+            TOON provides better compression ratio and as it a format optimized for information concisely to LLMs hence requiring fewer tokens than it's JSON counterpart.
             """
 
   value_add = """ Toons is valuable due to the fact that LLM model usage is depending on $$$ per 1M input tokens or so, 
